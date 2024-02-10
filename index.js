f86899b3e81cf7c117a3b18cd874d3388fb69a40
gabriel prado <analise.dados.biel@gmail.com>
2024-02-10 12:53:12 -0300

    foi criado o html e js

---
+++ index.js
@@ -0,0 +1,16 @@
+let nomeInserido = prompt('insira seu nome completo')
+let idadeInserida = prompt('insira sua idade')
+
+alert('Olá: ' + nomeInserido)
+alert('a idade inserida foi: ' + idadeInserida)
+
+let numeroInserido = prompt('insira o ano em que voçe nasceu')
+let numeroInserido2 = prompt('insira o ano atual')
+console.log(parseInt(numeroInserido) - parseInt(numeroInserido2 ))
+
+
+
+let nome = prompt('inseria seu primeiro nome')
+let sobrenome = prompt('insira seu sobre nome')
+const nomecompleto = `${nome} ${sobrenome}`
+alert('seu nome é:' + nomecompleto)
